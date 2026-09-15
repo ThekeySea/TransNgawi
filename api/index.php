e6d9950eabb95098c4a638c9d1ce375a721842ae
ThekeySea <2azhnanprib@gmail.com>
2026-09-15 15:20:28 +0700

    Enable display errors on api index

--- api/index.php
+++ api/index.php
@@ -1,8 +1,16 @@
 <?php
 
-// Buat folder /tmp/views hanya jika belum ada
-if (!is_dir('/tmp/views')) {
-    mkdir('/tmp/views', 0755, true);
+// Tampilkan semua error PHP langsung ke layar browser
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+// Buat direktori sementara di /tmp jika belum ada
+$dirs = ['/tmp/views', '/tmp/cache', '/tmp/sessions'];
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
 }
 
 require __DIR__ . '/../public/index.php';
