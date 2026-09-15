563548f1deffe38cbcf12419d3b30ea9b4c3c578
ThekeySea <2azhnanprib@gmail.com>
2026-09-15 15:16:49 +0700

    Fix mkdir warning in api index

--- api/index.php
+++ api/index.php
@@ -1,6 +1,8 @@
 <?php
 
-// Buat direktori sementara untuk kompilasi Blade & Cache Vercel
-mkdir('/tmp/views', 0755, true);
+// Buat folder /tmp/views hanya jika belum ada
+if (!is_dir('/tmp/views')) {
+    mkdir('/tmp/views', 0755, true);
+}
 
 require __DIR__ . '/../public/index.php';
